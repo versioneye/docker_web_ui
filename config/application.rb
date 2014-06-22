31f87c09c6ee5017106f091d7dd4c13ebc4fbd66
reiz <robert.reiz.81@gmail.com>
2014-06-22 23:08:11 +0200

    Remove active record

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
 require 'docker'
 
 # Require the gems listed in Gemfile, including any gems
