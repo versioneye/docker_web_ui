8ae50f0283cef2fcb7cdd6b28632882193d390f2
reiz <robert.reiz.81@gmail.com>
2014-06-22 23:10:52 +0200

    Change default port to 9999

--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -27,7 +27,7 @@ worker_processes 2
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 # listen "/path/to/.unicorn.sock", :backlog => 64
-listen 8080, :tcp_nopush => true
+listen 9999, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 60
