7bc98eced1fe75af3f0dc37f495dca675b2d45fb
reiz <robert.reiz.81@gmail.com>
2014-06-23 19:00:00 +0200

    Refactor for remote images

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -16,7 +16,9 @@ class ContainersController < ApplicationController
 
   def start
     cid = params['id']
-    opt = {'PortBindings' => { "11211/tcp" => [{"HostPort" => "11211"}] } } # TODO
+    image_name = image_name_for cid
+    config = config_for image_name
+    opt = config['container_start_opts']
     Docker.connection.post("/containers/#{cid}/start", nil, :body => opt.to_json)
     flash[:success] = "Container started."
     redirect_to containers_index_path
@@ -47,5 +49,30 @@ class ContainersController < ApplicationController
     end
     redirect_to containers_index_path
   end
+
+  private
+
+    def image_name_for id
+      image_name = ""
+      containers = JSON.parse Docker.connection.get('/containers/json?all=1')
+      containers.each do |container|
+        if container['Id'].to_s.eql?(cid.to_s)
+          image_name = container['Image']
+          break
+        end
+      end
+      image_name
+    end
+
+    def config_for image_name
+      config = ""
+      images = remote_images_hash
+      images.each do |key, value|
+        if key.eql?( image_name )
+          config = images[key]
+        end
+      end
+      config
+    end
 
 end
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -42,28 +42,4 @@ class ImagesController < ApplicationController
     end
   end
 
-  private
-
-    def remote_images_hash
-      response = fetch_response ENV['REMOTE_IMAGES']
-      JSON.parse response.body
-      # images = {}
-      # images['reiz/memcached:1.0.0']     = {'container_start_opts' => {'PortBindings' => { '11211/tcp' => [{'HostPort' => '11211'}]}}, 'comments' => 'First version' }
-      # images['reiz/mongodb:1.0.0']       = {'container_start_opts' => {'PortBindings' => { '27017/tcp' => [{'HostPort' => '27017'}]}, 'Binds' => ['/mnt/mongodb:/data']}, 'comments' => 'First version' }
-      # images['reiz/elasticsearch:1.0.0'] = {'container_start_opts' => {'PortBindings' => { '9200/tcp'  => [{'HostPort' => '9200'}], '9300' => [{'HostPort' => '9200'}]}, 'Binds' => ['/mnt/elasticsearch:/data']}, 'comments' => 'First version' }
-      # images
-    end
-
-    def fetch_response url
-      uri  = URI.parse url
-      http = Net::HTTP.new uri.host, uri.port
-      path  = uri.path
-      query = uri.query
-      http.get("#{path}?#{query}")
-    rescue => e
-      Rails.logger.error e.message
-      Rails.logger.error e.backtrace.join("\n")
-      nil
-    end
-
 end
