29cbc4336f52e4f06a758a8c3126fed73ed6dd12
reiz <robert.reiz.81@gmail.com>
2014-06-23 18:26:00 +0200

    Add remote_images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -22,7 +22,6 @@ class ImagesController < ApplicationController
     Thread.new{
       begin
         uri = "/images/create?fromImage=#{name}&tag=#{tag}"
-        p "POST #{uri}"
         dc = Docker.connection.post( uri )
       rescue => e
         p "error for POST /images/create?fromImage=#{name}&tag=#{tag}"
@@ -35,13 +34,10 @@ class ImagesController < ApplicationController
   end
 
   def remote_images
-    images = remote_images_hash
+    response = fetch_response ENV['REMOTE_IMAGES']
     respond_to do |format|
       format.json {
-        render :json => images.to_json
-      }
-      formmat.html {
-        @r_images = images
+        render :json => response.body
       }
     end
   end
@@ -49,11 +45,13 @@ class ImagesController < ApplicationController
   private
 
     def remote_images_hash
-      images = {}
-      images['reiz/memcached:1.0.0']     = {'container_start_opts' => {'PortBindings' => { '11211/tcp' => [{'HostPort' => '11211'}]}}, 'comments' => 'First version' }
-      images['reiz/mongodb:1.0.0']       = {'container_start_opts' => {'PortBindings' => { '27017/tcp' => [{'HostPort' => '27017'}]}, 'Binds' => ['/mnt/mongodb:/data']}, 'comments' => 'First version' }
-      images['reiz/elasticsearch:1.0.0'] = {'container_start_opts' => {'PortBindings' => { '9200/tcp'  => [{'HostPort' => '9200'}], '9300' => [{'HostPort' => '9200'}]}, 'Binds' => ['/mnt/elasticsearch:/data']}, 'comments' => 'First version' }
-      images
+      response = fetch_response ENV['REMOTE_IMAGES']
+      JSON.parse response.body
+      # images = {}
+      # images['reiz/memcached:1.0.0']     = {'container_start_opts' => {'PortBindings' => { '11211/tcp' => [{'HostPort' => '11211'}]}}, 'comments' => 'First version' }
+      # images['reiz/mongodb:1.0.0']       = {'container_start_opts' => {'PortBindings' => { '27017/tcp' => [{'HostPort' => '27017'}]}, 'Binds' => ['/mnt/mongodb:/data']}, 'comments' => 'First version' }
+      # images['reiz/elasticsearch:1.0.0'] = {'container_start_opts' => {'PortBindings' => { '9200/tcp'  => [{'HostPort' => '9200'}], '9300' => [{'HostPort' => '9200'}]}, 'Binds' => ['/mnt/elasticsearch:/data']}, 'comments' => 'First version' }
+      # images
     end
 
     def fetch_response url
