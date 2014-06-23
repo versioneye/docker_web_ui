1afe5b80d3783876063acf0176bcc871bf39ad26
reiz <robert.reiz.81@gmail.com>
2014-06-24 01:24:03 +0200

    Remove unused code

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -4,12 +4,6 @@ module ImagesHelper
   def remote_images_hash
     response = fetch_response ENV['REMOTE_IMAGES']
     JSON.parse response.body
-    # images = {}
-    # images['reiz/memcached:1.0.0']     = {'container_start_opts' => {'PortBindings' => { '11211/tcp' => [{'HostPort' => '11211'}]}}, 'comments' => 'First version' }
-    # images['reiz/mongodb:1.0.0']       = {'container_start_opts' => {'PortBindings' => { '27017/tcp' => [{'HostPort' => '27017'}]}, 'Binds' => ['/mnt/mongodb:/data']}, 'comments' => 'First version' }
-    # images['reiz/elasticsearch:1.0.0'] = {'container_start_opts' => {'PortBindings' => { '9200/tcp'  => [{'HostPort' => '9200'}], '9300' => [{'HostPort' => '9200'}]}, 'Binds' => ['/mnt/elasticsearch:/data']}, 'comments' => 'First version' }
-
-    # images
   end
 
 
